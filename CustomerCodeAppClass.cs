b2a8aecf290777dff45dc606afc0fb7eaf71e04d
Tim <tljames@eagles.usi.edu>
2014-11-21 17:51:07 -0600

    First commit

---
+++ CustomerCodeAppClass.cs
@@ -0,0 +1,118 @@
+﻿/* Customer Code App Class
+ * This class will seperate the Customer Info into the pieces used to generate the Customer Code.
+ * Author: Tim James */
+
+using System;
+
+namespace CustomerCodeApp
+{
+    class CustomerCodeAppClass
+    {
+        // Data fields
+        private string custFullName;
+        private string custBirthdate;
+        private int custMonthNumber;
+        private string lastName;
+        private int birthYear;
+        private string purchMonth;
+
+        // Properties
+        public string CustFullNames
+        {
+            get
+            {
+                return custFullName;
+            }
+            set
+            {
+                custFullName = value;
+            }
+        }
+        public string CustBirthdate
+        {
+            get
+            {
+                return custBirthdate;
+            }
+            set
+            {
+                custBirthdate = value;
+            }
+        }
+        public int CustMonthNumber
+        {
+            get
+            {
+                return custMonthNumber;
+            }
+            set
+            {
+                custMonthNumber = value;
+            }
+        }
+        // Read only properites
+        public string LastName
+        {
+            get
+            {
+                ParseName(custFullName);
+                return lastName;
+            }
+        }
+        public int BirthYear
+        {
+            get
+            {
+                ParseBirthdate(custBirthdate);
+                return birthYear;
+            }
+        }
+        public string PurchMonth
+        {
+            get
+            {
+                ReturnMonth(custMonthNumber);
+                return purchMonth;
+            }
+        }
+        
+        // Default Constructor
+        public CustomerCodeAppClass()
+        {
+        }
+        
+        // Parameterized Constructor
+        public CustomerCodeAppClass(string fn, string bd, int mn)
+        {
+            custFullName = fn;
+            CustBirthdate = bd;
+            custMonthNumber = mn;
+        }
+        
+        // This method will parse last name from full name
+        public void ParseName(string fn)
+        {
+            string[] nameParts = fn.Split(' ');
+
+            lastName = nameParts[1];
+        }
+        
+        // This method will parse year of birth from birthdate
+        public void ParseBirthdate(string bd)
+        {
+            string[] dateParts = bd.Split('/');
+            
+            birthYear = Convert.ToInt32(dateParts[2]);
+        }
+        
+        // This method will determine month name from month number
+        public void ReturnMonth(int mn)
+        {
+            string[] monthNames = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+            monthNames[0] = monthNames[0].TrimStart('0');
+
+            purchMonth = monthNames[mn - 1];
+        }
+    }
+}
---
+++ CustomerCodeAppProgram.cs
@@ -0,0 +1,72 @@
+﻿/* Customer Code App
+ * This app will determine a customer code mailing label using: Customer Name, Birthdate,
+ * Month Subscription was purchased, and Zip Code.
+ * Author: Tim James */
+
+using System;
+
+namespace CustomerCodeApp
+{
+    class CustomerCodeAppProgram
+    {
+        static void Main()
+        {
+            string fullName;
+            string birthdate;
+            int monthNumber;
+            int zipCode;
+            string inValue;
+            char anotherCode = 'N';
+
+            DisplayInstructions();
+
+
+            do
+            {
+                GetCustomerInfo(out fullName, out birthdate, out monthNumber, out zipCode);
+                
+                CustomerCodeAppClass code = new CustomerCodeAppClass(fullName, birthdate, monthNumber);
+
+                Console.WriteLine("The Customer Code is: {0}{1}{2}{3}", code.LastName, code.BirthYear % 100, code.PurchMonth, zipCode % 100);
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Would you like to determine another Customer Code? (y or n)");
+                inValue = Console.ReadLine();
+                anotherCode = Convert.ToChar(inValue);
+                Console.Clear();
+            }
+            while ((anotherCode == 'Y') || (anotherCode == 'y'));
+
+        }
+        // This method will display the instructions
+        static void DisplayInstructions()
+        {
+            Console.WriteLine("Welcome to the Customer Code App!!");
+            Console.WriteLine("**********************************");
+            Console.WriteLine();
+            Console.WriteLine("This app will determine the Customer Code for the mailing label");
+            Console.WriteLine("that will appear on the Magazine.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to enter Customer Info...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        // This method will allow the user to enter the Customer Information
+        public static void GetCustomerInfo(out string fullName, out string birthdate, out int monthNumber, out int zipCode)
+        {
+            Console.WriteLine("Please Enter-");
+            Console.Write("Customer Name (First Last): ");
+            fullName = Console.ReadLine();
+            Console.Write("Customer Birthdate (mm/dd/yyyy): ");
+            birthdate = Console.ReadLine();
+            Console.Write("Date of Month subscription purchased (Number of Month): ");
+            monthNumber = int.Parse(Console.ReadLine());
+            Console.Write("Customer Zip Code (12345): ");
+            zipCode = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+    }
+}
